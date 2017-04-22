18e337f85d0a00060f130d1dfe8a5bc1c5eab270
NetFusion <terrorhund.the.chilla@hotmail.de>
2017-04-22 23:20:38 +0200

    work on enemy movement

---
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+    public int speed;
+    public float rotationSpeed;
+    public GameObject player;
+
+    Rigidbody rb;
+
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+    }
+	
+	void FixedUpdate() {
+        Vector3 moveDirection = Vector3.Normalize(this.player.transform.position - this.transform.position);
+        rb.MovePosition(rb.position + moveDirection * Time.deltaTime);
+        transform.LookAt(this.player.transform);
+    }
+}
