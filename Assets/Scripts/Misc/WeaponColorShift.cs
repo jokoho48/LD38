432395f43baf4a5808cebab6b48940850cfe74a1
jokoho48 <hoffmann.jonas95@gmail.com>
2017-04-22 19:12:15 +0200

    add Hand/Weapon
    add some Standart assests

---
+++ Assets/Scripts/Misc/WeaponColorShift.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponColorShift : MonoBehaviour
+{
+
+	public Material mat1;
+	public Color[] colors;
+	public Color color;
+	// Use this for initialization
+	void Start()
+	{
+		StartCoroutine("RandomColor");
+		StartCoroutine("FadeColor");
+	}
+
+	IEnumerator RandomColor()
+	{
+		while (true)
+		{
+
+			color = colors[Random.Range(0, colors.Length - 1)];
+			yield return new WaitForSeconds(5);
+		}
+	}
+
+	IEnumerator FadeColor()
+	{
+		while (true)
+		{
+			Color c = Color.Lerp(mat1.color, color, Time.deltaTime);
+			mat1.SetColor("_Color", c);
+			mat1.SetColor("_EmissionColor", c);
+			yield return new WaitForSeconds(0.3f);
+		}
+	}
+}
