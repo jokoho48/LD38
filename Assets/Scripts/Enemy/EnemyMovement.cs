03c88b38f700386471997225fbad431c65c23e56
NetFusion <terrorhund.the.chilla@hotmail.de>
2017-04-23 14:17:43 +0200

    enemy collision avoidance

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -8,17 +8,38 @@ public class EnemyMovement : MonoBehaviour {
     public GameObject player;
 
     Rigidbody rb;
+    Vector3 target;
 
 
 	void Start () {
-        rb = GetComponent<Rigidbody>();
-		rb.useGravity = false;
+        this.rb = GetComponent<Rigidbody>();
+        this.target = player.transform.position;
+    }
+
+    void Update() {
+        if (this.rb.velocity != Vector3.zero && Physics.Raycast(this.transform.position, this.rb.velocity, out this.hit, 100f)) {
+            this.target = this.hit.point + this.hit.normal * 5;
+        }
+        if (Vector3.Distance(this.transform.position, this.target) < 5) {
+            this.target = player.transform.position;
+        }
     }
 	
 	void FixedUpdate() {
-        Vector3 moveDirection = Vector3.Normalize(this.player.transform.position - this.transform.position);
-        rb.MovePosition(rb.position + moveDirection * Time.fixedDeltaTime * speed);
-
-        transform.rotation = Quaternion.LookRotation(moveDirection, PlayerController.GravObj.transform.up);
+        if (Vector3.Distance(this.transform.position, this.player.transform.position) > 10f) {
+            Vector3 moveDirection = Vector3.Normalize(this.target - this.transform.position);
+            rb.velocity = moveDirection * this.speed;
+        }
+        
+        this.transform.rotation = Quaternion.RotateTowards(this.transform.rotation, Quaternion.LookRotation(this.rb.velocity, PlayerController.GravObj.transform.up), this.rotationSpeed);
 	}
+
+    RaycastHit hit;
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(this.transform.position, hit.point);
+        Gizmos.DrawLine(hit.point, hit.point + hit.normal * 10);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(this.transform.position, this.transform.position + this.rb.velocity * this.speed);
+    }
 }
